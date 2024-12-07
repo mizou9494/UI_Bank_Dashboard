ccddfcaac77923cc815167917f2636bc2a3ded69
mizou9494 <betarmizou@gmail.com>
2024-12-07 23:54:29 +0100

    the account page is getting build up slowly but steadily

--- src/components/Accounts/Accounts.js
+++ src/components/Accounts/Accounts.js
@@ -1,9 +1,51 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import { TbMoneybag } from "react-icons/tb"
 
 function Accounts() {
-  return <h1 className='flex justify-center items-center gap-3 text-red-600 text-3xl'>Accouuuuuunts <TbMoneybag /> </h1>;
+  return (
+    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+      <Card>
+        <IconSide>
+          <TbMoneybag color='yellow' size={30} />
+        </IconSide>
+        <OtherSide>
+          <span>Accounts</span>
+          <span>$5,600</span>
+        </OtherSide>
+      </Card>
+    </div>
+  )
 }
+
+const Card = styled.div`
+  display: flex;
+  flex: 1;
+`
+
+const IconSide = styled.div`
+  display: grid;
+  place-content: center;
+  background-color: #EAF0FF;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`
+
+const OtherSide = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & span:nth-child(1) {
+    color: gray;
+  }
+
+  & span:nth-child(2) {
+    font-weight: 600;
+    font-size: 20px;
+  }
+`
 
 export default Accounts;
