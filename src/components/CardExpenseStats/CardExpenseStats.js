56a7b3a94b6e413cee78640f8e6c417360faa8aa
mizou9494 <betarmizou@gmail.com>
2024-12-15 22:44:53 +0100

    Adjusting the Pie Chart for better looks

--- src/components/CardExpenseStats/CardExpenseStats.js
+++ src/components/CardExpenseStats/CardExpenseStats.js
@@ -3,17 +3,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ReactEcharts from 'echarts-for-react'
+import { Radius } from 'lucide-react';
 
 function CardExpenseStats() {
   const option = {
-    tooltip: {},
-    legend: {
-      display: false
+    tooltip: {
+      trigger: "item"
     },
-    yAxis: {},
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    // grid: {
+    //   left: '10%',
+    //   right: '10%',
+    //   bottom: '10%',
+    //   containLabel: true,
+    // },
+    // yAxis: {},
     series: [
       {
+        name: 'Bank Distribution',
         type: 'pie',
+        radius: ['50%', '70%'],
         data: [
           {
             value: 100,
@@ -31,11 +43,48 @@ function CardExpenseStats() {
             value: 300,
             name: 'Access Bank'
           }
-        ]
+        ],
+        roseType: 'area',
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        },
       }
     ]
   };
-
+  // option = {
+  //   series: [
+  //     {
+  //       type: 'pie',
+  //       data: [
+  //         {
+  //           value: 100,
+  //           name: 'A'
+  //         },
+  //         {
+  //           value: 200,
+  //           name: 'B'
+  //         },
+  //         {
+  //           value: 300,
+  //           name: 'C'
+  //         },
+  //         {
+  //           value: 400,
+  //           name: 'D'
+  //         },
+  //         {
+  //           value: 500,
+  //           name: 'E'
+  //         }
+  //       ],
+  //       roseType: 'area'
+  //     }
+  //   ]
+  // };
   return (
     <>
       <SectionHead>Card Expense Statistics</SectionHead>
