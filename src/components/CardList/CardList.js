b3bdd7de3ff0e08db66ce0c4a7236610a0178f6f
mizou9494 <betarmizou@gmail.com>
2024-12-19 22:46:37 +0100

    working on the Credit Card's page

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 function CardList() {
-  return <div></div>;
+  return (
+    <>
+      <SectionHead>Card List</SectionHead>
+      <CardListWrapper>
+        <ListItem>
+          Item 1
+        </ListItem>
+        <ListItem>
+          Item 2
+        </ListItem>
+        <ListItem>
+          Item 3
+        </ListItem>
+      </CardListWrapper>  
+    </>
+  )
 }
+
+const CardListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const ListItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const SectionHead = styled.h2`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+`
 
 export default CardList;
